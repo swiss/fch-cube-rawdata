bb36cf0f7c3394e9923828061a4fc0b82a411c80
Michael Jehle <michael.jehle@bit.admin.ch>
2025-11-24 13:44:57 +0100

    chore: layout of comments improved

--- src/Swiss.FCh.Cube.RawData/Extensions/BkCubeRawDataServiceCollectionExtensions.cs
+++ src/Swiss.FCh.Cube.RawData/Extensions/BkCubeRawDataServiceCollectionExtensions.cs
@@ -1,13 +1,14 @@
+// ReSharper disable UnusedType.Global
+// ReSharper disable UnusedMember.Global
+
 using Swiss.FCh.Cube.RawData.Contract;
 using Swiss.FCh.Cube.RawData.Services;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Swiss.FCh.Cube.RawData.Extensions
 {
-// ReSharper disable once UnusedType.Global : Used by the library's consumers
     public static class BkCubeRawDataServiceCollectionExtensions
     {
-        // ReSharper disable once UnusedMember.Global : Used by the library's consumers
         public static IServiceCollection AddRawDataService(this IServiceCollection services)
         {
             services.AddScoped<ICubeRawDataService, RawDataService>();
